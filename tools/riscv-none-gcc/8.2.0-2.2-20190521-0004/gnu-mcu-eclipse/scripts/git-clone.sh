2800ff6da70cf129381454e2c1ebbf85f6101562
xiaoming <2014500726@smail.xtu.edu.cn>
2021-03-28 12:07:03 +0800

    Remove Compiler Toolchains

--- tools/riscv-none-gcc/8.2.0-2.2-20190521-0004/gnu-mcu-eclipse/scripts/git-clone.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-rm -rf "${HOME}/Downloads/riscv-none-gcc-build.git"
-git clone --recurse-submodules https://github.com/gnu-mcu-eclipse/riscv-none-gcc-build.git "${HOME}/Downloads/riscv-none-gcc-build.git"
